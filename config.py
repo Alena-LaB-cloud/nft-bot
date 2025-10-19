8b6cf4c7153e9e642d64f88530c0c01ab57b9495
Alena-LaB-cloud <alenaspiridonova2@mail.ru>
2025-10-19 21:31:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,3 @@ ADMIN_IDS = [int(x.strip()) for x in os.getenv('ADMIN_IDS', '788630583').split('
 MIN_NFT_PRICE = 0.1
 MAX_NFT_PRICE = 100.0
 TON_NETWORK = 'testnet'
-
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8429039115:AAFLkJFjhgbpMyva7Kf5fHydDOVIPWdRCdc')
-ADMIN_IDS = [int(x.strip()) for x in os.getenv('ADMIN_IDS', '788630583').split(',')]
-MIN_NFT_PRICE = 0.1
-MAX_NFT_PRICE = 100.0
-TON_NETWORK = 'testnet'
